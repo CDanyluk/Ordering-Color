5bca759c1c525a930c4df59874991b84088e4b53
CDanyluk <chantaldanyluk@yahoo.com>
2018-09-11 18:38:44 -0500

    Creation of RGBPoint to store integers r, g, b and RGBGraph to represent connections between these points in the future.

---
+++ WindowsFormsApp1/RGBGraph.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Drawing;
+
+
+namespace OrderColors
+{
+    class RGBGraph
+    {
+
+        private List<RGBPoint> points = new List<RGBPoint>();
+
+        public RGBGraph(List<Color> colorlist)
+        {
+            for (int i = 0; i < colorlist.Count; i++)
+            {
+                colorlist[i].ToArg
+            }
+    }
+}
